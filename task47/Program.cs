71260a4a4239a6565c6aa0a9bb0ea81ce718bc37
MalaFerre <malaferree@outlook.com>
2023-03-10 09:50:42 +0300

    task47

---
+++ task47/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+ 8 7,8 -7,1 9
+*/
+
+int m = ReadInt("Введите количетсво строк матрицы: ");
+int n = ReadInt("Введите количество столбцов матрицы: ");
+double[,] matrix = new double[m, n];
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void GenerateMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Convert.ToDouble(new Random().Next(-1000, 1000)) / 100;
+        }
+    }
+}
+
+void PrintMatrix(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            
+            System.Console.Write(matr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+GenerateMatrix(matrix);
+PrintMatrix(matrix);
